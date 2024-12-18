4c67dd79e6fac60a6dc71892275de98f18669fcc
sodamkimkim <theka265@gmail.com>
2024-12-18 11:05:53 +0900

    커스텀 getter, setter, 프로퍼티처럼 사용

--- src/com/lannstark/lec09/main.kt
+++ src/com/lannstark/lec09/main.kt
@@ -5,25 +5,34 @@ fun main() {
     println(person.toString());
 }
 
-class Person(private val name: String, private var age: Int) { // end of class
+class Person(name: String = "홍길동", age: Int = 10) { // end of class
+    var name = name
+        get() = field.uppercase(); // backing field
+        set(value) {
+            field = value.uppercase();
+        }
+    fun getUppercaseName(): String = this.name.uppercase(); // backing field 대신 this.name 사용
+
+    var age = age;
+
     init {
         if (age <= 0)
             throw IllegalArgumentException("나이는 ${age}일 수 없습니다.");
         println("주생성자");
     }
 
-    public fun getAge(): Int {
-        return age; }
 
     override fun toString(): String {
         return "Person(name='$name', age=$age)"
     }
 
-    constructor(name: String) : this(name, 1) {
-        println("첫번째 부생성자");
+    // 함수로 만들기
+    fun isAdult(): Boolean {
+        return this.age >= 20;
     }
 
-    constructor() : this("홍길동") {
-        println("두번째 부생성자");
-    };
+    // 프로퍼티처럼 만들기 (Custom Getter)
+    val isAdult: Boolean
+        get() = this.age > 20;
+
 } // end of class
